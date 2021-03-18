640570f2bd3a111cca191bd8ef5f37ae3eba5186
Leah Lundqvist <leah@pigeon.sh>
2021-03-18 19:01:31 +0100

    add provisioning to readme

--- README.md
+++ README.md
@@ -16,3 +16,14 @@ team_id=APPLETEAMID
 codesign_identity=Apple Development: Leah Lundqvist (IDENTITY_ID)
 provisioning_profile=app_vlang_helloworld.mobileprovision
 ```
+
+### Provision Profile Wizard
+
+To enable automated provisioning, you need to create an API key on [App Store Connect](https://appstoreconnect.apple.com/access/api). Save the key file to `~/.vib` and create a file called `config.vib` in the same directory containing
+```
+issuer_id=<App Store Connect Issuer ID>
+key_id=<Key ID>
+key_file=AuthKey_<Key ID>.p8
+```
+
+Use `vib provision` to enter the provisioning profile wizard. For now you need to already have codesigning certificate on your machine. Generating and installing a new one will be automated by Vib in the future as well.
--- appstoreconnect/jwt.v
+++ appstoreconnect/jwt.v
@@ -1,40 +1,40 @@
-module appstoreconnect
+// module appstoreconnect
 
-import json
-import time
-import encoding.base64
+// import json
+// import time
+// import encoding.base64
 
-struct JWTHeader {
-	alg string = 'HS256'
-	kid string
-	typ string = 'JWT'
-}
+// struct JWTHeader {
+// 	alg string = 'HS256'
+// 	kid string
+// 	typ string = 'JWT'
+// }
 
-struct JWTPayload {
-	iss string
-	exp u64
-	aud string = "appstoreconnect-v1"
-}
+// struct JWTPayload {
+// 	iss string
+// 	exp u64
+// 	aud string = "appstoreconnect-v1"
+// }
 
-fn base64urlencode(b []byte) string {
-	return base64.encode(b).replace('+', '-').replace('/', '_').replace('=', '')
-}
+// fn base64urlencode(b []byte) string {
+// 	return base64.encode(b).replace('+', '-').replace('/', '_').replace('=', '')
+// }
 
-fn (mut c ConnectConfig) create_jwt() {
-	jwt_header := base64urlencode(json.encode(JWTHeader {
-		kid: c.key_id
-	}).bytes())
+// fn (mut c ConnectConfig) create_jwt() {
+// 	jwt_header := base64urlencode(json.encode(JWTHeader {
+// 		kid: c.key_id
+// 	}).bytes())
 
-	exp := time.now().unix + 60 * 5 // now + 5 minutes
+// 	exp := time.now().unix + 60 * 5 // now + 5 minutes
 
-	jwt_payload := base64urlencode(json.encode(JWTPayload {
-		iss: c.issuer_id
-		exp: exp
-	}).bytes())
+// 	jwt_payload := base64urlencode(json.encode(JWTPayload {
+// 		iss: c.issuer_id
+// 		exp: exp
+// 	}).bytes())
 
-	sig_payload := "${jwt_header}.${jwt_payload}"
+// 	sig_payload := "${jwt_header}.${jwt_payload}"
 
-	sig := ''.bytes() //TODO: implement crypto.ecdsa
+// 	sig := ''.bytes() //TODO: implement crypto.ecdsa
 
-	c.jwt = '${jwt_header}.${jwt_payload}.${base64urlencode(sig)}'
-}
+// 	c.jwt = '${jwt_header}.${jwt_payload}.${base64urlencode(sig)}'
+// }
